3706a16400e08bcb3a98d7baaa6b1c2d2fa23273
Carlos <carlosiego2017@gmail.com>
2023-05-02 22:31:28 -0300

    adding lib fs and contidion of platform

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ dotenv.config()
 import express from 'express'
 import routes from './routes/index.js'
 import oracledb from 'oracledb'
+import fs from 'fs'
 
-let libPath = 'C:\\instantclient';
-oracledb.initOracleClient({ libDir: libPath })
+let libPath;
+if (process.platform === 'win32') {           // Windows
+  libPath = 'C:\\instantclient';
+} else if (process.platform === 'darwin') {   // macOS
+  libPath = process.env.HOME + '/Documentos/instantclient';
+}
+
+if (libPath && fs.existsSync(libPath)) {
+  oracledb.initOracleClient({ libDir: libPath });
+}
+
 
 const app = express()
 app.use(express.json())
