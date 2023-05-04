8b7a621b3cf8d5e883be490fa8b0594d1c431330
Carlos <carlosiego2017@gmail.com>
2023-05-04 17:25:57 -0300

    add string prototype replaceAll

--- src/controllers/transformReqString.js
+++ src/controllers/transformReqString.js
@@ -1,3 +1,9 @@
+if(typeof String.prototype.replaceAll === "undefined") {
+    String.prototype.replaceAll = function(match, replace) {
+       return this.replace(new RegExp(match, 'g'), () => replace);
+    }
+}
+
 const transformReqString = (description) => {
     let descriptionMod = description.toUpperCase()
     descriptionMod = descriptionMod.replaceAll(' ', '%')
