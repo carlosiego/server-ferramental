a0a947d40e02955a6ddcb23ee55318f37c762fd6
Carlos <carlosiego2017@gmail.com>
2023-05-09 17:58:51 -0300

    message error no products with this description

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -17,6 +17,7 @@ class productsController {
             }else{
                 res.status(400).json({
                     error: true,
+                    message: `Erro, não existe produto com a descrição ${description}`,
                     products: []
                 })  
             }
