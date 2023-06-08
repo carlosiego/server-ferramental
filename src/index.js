d34316303af339bef7aab30cf9546878b854f9b8
Carlos <carlosiego2017@gmail.com>
2023-06-08 17:39:28 -0300

    create file initoracledb, and update in index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,32 @@
 require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
-const oracledb = require('oracledb')
-const fs = require('fs')
+const initializeOracleDB = require('./database/initializeOracleDB')
+initializeOracleDB()
 
 const productsRouter = require('./routes/productsRoutes')
 const salesOrdersRouter = require('./routes/salesOrdersRoutes')
 const budgetsRouter = require('./routes/budgetsRoutes')
 
-let libPath;
-if (process.platform === 'win32') {           // Windows
-  libPath = 'C:\\instantclient';
-} else if (process.platform === 'linux') {   // Linux
-  libPath = process.env.HOME + '/Documentos/instantclient_linux';
-}
-
-if (libPath && fs.existsSync(libPath)) {
-  oracledb.initOracleClient({ libDir: libPath });
-  console.log('Instant oracle inited with sucess !')
-}
-
 const app = express()
 
 app.use(express.json())
+
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
+
 app.use(productsRouter, salesOrdersRouter, budgetsRouter)
+
 app.use((error, req, res, next) => {
+
   console.log('============= ERROR HANDLER =============')
   console.log(error)
   res.sendStatus(500)
+
 })
 
 app.listen(process.env.PORT, () => {
