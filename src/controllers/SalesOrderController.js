5e9eb9c8bc490be1f41197d6fd229d09796428c0
carlosiego <carlosiego2017@gmail.com>
2023-10-21 14:07:43 -0300

    add cache in showByNum in SalesOrderController

--- src/controllers/SalesOrderController.js
+++ src/controllers/SalesOrderController.js
@@ -1,14 +1,22 @@
 const SalesOrdersRepository = require('../repositories/SalesOrdersRepository')
+const client = require('../redis')
 
 class SalesOrderController {
 
 	async showByNum(req, res) {
 
 		let numberOrder = req.params.numberorder
+		let fullUrl =  req.protocol + '://' + req.get('host') + req.originalUrl;
+
+		let salesOrderFromCache = await client.get(fullUrl)
+		if (salesOrderFromCache) {
+			return res.json(JSON.parse(salesOrderFromCache))
+		}
 
 		let [headerOrder, prodsOrder] = await SalesOrdersRepository.findByNumOrder(numberOrder)
 
 		if (!prodsOrder.rows.length) {
+			await client.set(fullUrl, JSON.stringify({ error: 'Pedido de venda não encontrado' }), { EX: process.env.EXPIRATION})
 			return res.status(404).json({ error: 'Pedido de venda não encontrado' })
 		}
 
@@ -27,6 +35,7 @@ class SalesOrderController {
 			products.push(prod)
 		})
 
+		await client.set(fullUrl, JSON.stringify({ headers, products }), { EX: process.env.EXPIRATION})
 		res.json({ headers, products })
 
 	}
