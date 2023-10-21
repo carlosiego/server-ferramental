65e2017683ccad2a31b8fa689687db2ff0f00c95
carlosiego <carlosiego2017@gmail.com>
2023-10-21 13:50:01 -0300

    cache in budget showByNum

--- src/controllers/BudgetController.js
+++ src/controllers/BudgetController.js
@@ -5,10 +5,15 @@ class BudgetController {
 	async showByNum(req, res) {
 
 		let numberBudget = req.params.numberbudget
-
+		let fullUrl =  req.protocol + '://' + req.get('host') + req.originalUrl;
+		let budgetFromCache = await client.get(fullUrl)
+		if (budgetFromCache) {
+			return res.json(JSON.parse(budgetFromCache))
+		}
 		let [headersBudget, prodsBudget] = await BudgetsRepository.findByNumber(numberBudget)
 
 		if (!prodsBudget.rows.length) {
+			await client.set(fullUrl, JSON.stringify({ error: 'Orçamento não encontrado' }), { EX: process.env.EXPIRATION })
 			return res.status(404).json({ error: 'Orçamento não encontrado' })
 		}
 
@@ -26,7 +31,7 @@ class BudgetController {
 			})
 			products.push(prod)
 		})
-
+		await client.set(fullUrl, JSON.stringify({ headers, products }), { EX: process.env.EXPIRATION })
 		res.json({ headers, products })
 
 	}
