7ee6b8327f9d036936246916dfe27cec76c928cb
Carlos <carlosiego2017@gmail.com>
2024-04-06 11:02:50 -0300

    FEAT: 100% showUsersWithRca

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,9 +5,32 @@ class UserController {
 
 	async showUsersWithRca(req, res) {
 
-		let users = await UsersRepository.findUsersWithRca()
+		let fullUrl =  req.protocol + '://' + req.get('host') + req.originalUrl;
+		let usersFromCache = await client.get(fullUrl)
 
-		return res.json({ users })
+		if (usersFromCache) {
+			return res.json(JSON.parse(usersFromCache))
+		}
+
+		let { metaData, rows} = await UsersRepository.findUsersWithRca()
+
+		if(!rows.length) {
+			await client.set(fullUrl, JSON.stringify({error: 'Usuários não encontrados'}), { EX: process.env.EXPIRATION})
+			return res.status(404).json({error: 'Usuários não encontrados'})
+		}
+
+		let users = []
+
+		rows.forEach(user => {
+			let prop = {}
+			user.forEach((value, index) => {
+				prop[metaData[index].name] = value
+			})
+			users.push(prop)
+		})
+
+		await client.set(fullUrl, JSON.stringify(users), { EX: process.env.EXPIRATION })
+		return res.json(users)
 	}
 }
 
